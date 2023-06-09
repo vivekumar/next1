edfa2b8b9fff3b54a2fc4edaad7d6f3f40e99ee1
Vivek <vivek.kumar@rxcs.in>
2023-06-09 19:03:34 +0530

    add cart

--- src/app/layout.js
+++ src/app/layout.js
@@ -39,13 +39,13 @@ export default function RootLayout({ children }) {
               Cancel Preloader</span>
           </div>
   </div>*/}
-
-        <div id="main-wrapper" className="main-wrapper">
-          <Header />
-          {children}
-          <Footer />
-        </div>
-
+        <Provider store={store}>
+          <div id="main-wrapper" className="main-wrapper">
+            <Header />
+            {children}
+            <Footer />
+          </div>
+        </Provider>
         <div className="rn-progress-parent">
           <svg className="rn-back-circle svg-inner" width="100%" height="100%" viewBox="-1 -1 102 102">
             <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
